36c7ced4ee837adc2533590c0c22dc1b392b46a6
Evon Perez <perez.evon@gmail.com>
2021-06-25 18:06:44 -0400

    evons changes

--- app.js
+++ app.js
@@ -12,12 +12,26 @@ const verb1 = document.getElementById('verb-1');
 const verb2 = document.getElementById('verb-2');
 const adjective1 = document.getElementById('adjective-1');
 const noun3 = document.getElementById('noun-3');
+
+const lyrics = document.getElementById('lyrics');
+
+let array1 = [noun1Input, noun2Input, verb1Input, verb2Input, adjective1Input, noun3Input];
+
+let array2 = [noun1, noun2, verb1, verb2, adjective1, noun3];
+
  
 submitButton.addEventListener('click', change);
 
-function change() {
-    noun1.textContent = noun1Input.value;
-    noun2.textContent 
+function change(){
+
+  lyrics.style.visibility = 'visible';
+
+for(var i = 0; i < array1.length; i++ ){
+
+array2[i].textContent = array1[i].value.toUpperCase();
+
+
+}
 
 }
 
