341bd493d894f06085714de07240c96d83a24796
TANNER MECK <steve.meck246@gmail.com>
2021-06-25 14:30:15 -0700

    tanners changes

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // import functions
+const noun1Input = document.getElementById('noun-1-input');
+const noun2Input = document.getElementById('noun-2-input');
+const verb1Input = document.getElementById('verb-1-input');
+const verb2Input = document.getElementById('verb-2-input');
+const adjective1Input = document.getElementById('adjective-1-input');
+const noun3Input = document.getElementById('noun-3-input');
+const submitButton = document.getElementById('submit-button');
+const noun1 = document.getElementById('noun-1');
+const noun2 = document.getElementById('noun-2');
+const verb1 = document.getElementById('verb-1');
+const verb2 = document.getElementById('verb-2');
+const adjective1 = document.getElementById('adjective-1');
+const noun3 = document.getElementById('noun-3');
+ 
+submitButton.addEventListener('click', change);
 
-// reference needed DOM elements
+function change() {
+    noun1.textContent = noun1Input.value;
+    noun2.textContent 
+
+}
 
 
 // set event listeners 
